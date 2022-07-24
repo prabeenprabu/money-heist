35d7650074bcee3cce06b327f3c424cceaaacbb0
prabeen <74868630+prabeenprabu@users.noreply.github.com>
2022-07-24 06:36:33 -0400

    Add files via upload

---
+++ script/script.js
@@ -0,0 +1,20 @@
+let u=document.getElementById("dis");
+
+function showMenu()
+{
+    u.style.left= "-10px";
+}
+
+function hideMenu()
+{
+    u.style.left= "-400px";
+}
+
+// let song=document.getElementById("songs");
+// let audio=document.getElementById("Audio");
+// // song.autoplay;
+
+// window.onload = function()
+// {
+//     audio.play();
+// }
